ad59f44ae9936c731eef48fcb2cc5c933fcc1a76
Josiah Humphrey <thedarkshard99@gmail.com>
2018-09-24 14:52:16 -0400

    Revert

--- config.rb
+++ config.rb
@@ -12,7 +12,6 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-page '/apply.html', :layout => 'apply'
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
